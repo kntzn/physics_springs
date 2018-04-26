ca4f34b407e8b927c42f7aace9fe184f845b00f8
kntzn <kntznart@gmail.com>
2018-04-26 15:50:35 +0300

    [Add]: second body

--- Engine/Engine/example1/main.cpp
+++ Engine/Engine/example1/main.cpp
@@ -22,30 +22,24 @@ int main ()
 	// Main window
 	sf::RenderWindow window (sf::VideoMode (1600, 900), "");
 	
-	// Position radius-vectors
-	FloatVector2D r1 (100, 100);
-	FloatVector2D r2 (200, 200);
-	// Velocity vectors
-	FloatVector2D V1 (10, 10);
-	FloatVector2D V2 (10, 5);
-	// Force vectors
-	FloatVector2D F1 (0, 0);
-	FloatVector2D F2 (0, 0);
+	// Position radius-vector
+	FloatVector2D r (100, 100);
+	// Velocity vector
+	FloatVector2D V (10, 10);
+	// Force vector
+	FloatVector2D F (0, 0);
 	// Time step value (1/60 of second)
 	const float dt = 0.016f;
-	// Masses of the bodies
-	const float mass1 = 1.f;
-	const float mass2 = 1.f;
+	// Mass of the body
+	const float mass = 1.f;
 
 	while (window.isOpen ())
 		{
-		
-		updatePoint (r1, V1, F1, dt, mass1);
-		updatePoint (r2, V2, F2, dt, mass2);
-
 		window.clear ();
-		drawPoint (r1, window);
-		drawPoint (r2, window);
+
+		updatePoint (r, V, F, dt, mass);
+		drawPoint (r, window);
+
 		window.display ();
 		}
 
